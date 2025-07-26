a361ad29a6904cb675545fbca948a7779facf674
Ocean <ocean.zhang.123@gmail.com>
2025-07-25 19:24:51 -0700

    Update decompiler tool and scripts

--- cmd_decompiler.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export ROOT=$(pwd)
-
-MTL_HUD_ENABLED=1 OBJC_DEBUG_MISSING_POOLS=NO MTL_CAPTURE_ENABLED=0 WINEPREFIX=$ROOT/wineprefix XDG_DATA_HOME=$ROOT/.local/share XDG_CONFIG_HOME=$ROOT/.config \
-XDG_CACHE_HOME=$ROOT/.cache WINEDEBUG="fixme-all" $ROOT/toolchains/wine/bin/wine  $ROOT/decompiler_tool/Debug/cmd_Decompiler.exe "$@"
---
+++ cmd_decompiler_v5.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export ROOT=$(pwd)
+
+MTL_HUD_ENABLED=0 OBJC_DEBUG_MISSING_POOLS=NO WINEPREFIX=$ROOT/wineprefix XDG_DATA_HOME=$ROOT/.local/share XDG_CONFIG_HOME=$ROOT/.config \
+XDG_CACHE_HOME=$ROOT/.cache WINEDEBUG="fixme-all" WINEDLLOVERRIDES="dxgi,d3d11,d3dcompiler_47=n,b" $ROOT/toolchains/wine/bin/wine  $ROOT/decompiler_tool/Debug/cmd_Decompiler.exe "$@"
